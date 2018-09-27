4399ed5da1b9e5d07fa7436a177c573aa626d90d
Ivan <bukunivan23@gmail.com>
2018-09-27 20:14:53 +0300

    With delete

--- ConvNetworks/Convolution.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def SingleSlice(tensor, weights, bias):
-    return np.sum((tensor * weights) + bias)
--- ConvNetworks/Pool.py
+++
@@ -1,4 +0,0 @@
-def MaxPool(matrix):
-    return matrix.max()
-def AveragePool(matrix):
-    return int(matrix.mean())

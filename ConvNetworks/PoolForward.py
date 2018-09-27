96ecb4747e283888d3fd7a355a11871d6d923c22
Ivan <bukunivan23@gmail.com>
2018-09-27 20:49:38 +0300

    Simplified

--- ConvNetworks/PoolForward.py
+++ ConvNetworks/PoolForward.py
@@ -1,54 +1,54 @@
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-np.random.seed(1)
-
-def MaxPool(matrix):
-    return matrix.max()
-def AveragePool(matrix):
-    return int(matrix.mean())
+#np.random.seed(1)
 
 
-def PoolForward(a, f, parameter, hyperparameters):
+def PoolForward(a, hyperparameters, mode="max"):
     stride = hyperparameters["stride"]
+    f = hyperparameters['f']
 
     (m, nH, nW, nC) = a.shape  # Input shape of the tensor
 
-    newNH = int((nH - f) / stride) + 1
-    newNW = int((nH - f) / stride) + 1
+    newNH = int((nH - f) / stride) + 1  # Output height of the tensor
+    newNW = int((nH - f) / stride) + 1  # Output width of the tensor
 
-    p = np.zeros((m, newNH, newNW, nC), dtype=np.int32)
+    p = np.zeros((m, newNH, newNW, nC), dtype=np.int32)  # Output tensor
+
     for m1 in range(m):
         for i, h1 in enumerate(range(0, nH, stride)):
             for j, w1 in enumerate(range(0, nW, stride)):
                 for c1 in range(nC):
                     try:
-                        p[m1, i, j, c1] = MaxPool(
-                            a[m1, h1:h1 + f, w1:w1 + f, c1]) if parameter == "max" else AveragePool(
-                            a[m1, h1:h1 + f, w1:w1 + f, c1])
+                        if mode == "max":
+                            p[m1, i, j, c1] = np.max(a[m1, h1:h1 + f, w1:w1 + f, c1])
+                        elif mode == "average":
+                            p[m1, i, j, c1] = int(np.mean(a[m1, h1:h1 + f, w1:w1 + f, c1]))
                     except:
                         break
-    return p
+    cache = (a, hyperparameters, mode)  # Parameters for back propagation
 
+    return p, cache
 
+'''
 m = 1
 nH = 8
 nW = 8
 nC = 1
 poolSize = 2
 stride = 2
-hyperparameters ={"stride": stride}
+hyperparameters ={"stride": stride, 'f':poolSize}
 x = np.random.randint(0,3,(m,nH,nW,nC))
-
 
 fig, ax1 = plt.subplots(1,1)
 ax1.set_title("x{}".format(x.shape))
 ax1.imshow(x[0,:,:,0])
 
-p = PoolForward(x, poolSize, "max",hyperparameters)
+p,cache = PoolForward(x, hyperparameters)
 
 print("x:\n",x[0,:,:,0])
 print("p:\n",p[0,:,:,0])
 fig, ax1 = plt.subplots(1,1)
 ax1.set_title("p{}".format(p.shape))
 ax1.imshow(p[0,:,:,0])
+'''

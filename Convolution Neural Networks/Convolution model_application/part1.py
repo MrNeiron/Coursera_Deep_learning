413db6f0af19fce3a52ee01812d14cf0a99cd4e5
Ivan <bukunivan23@gmail.com>
2018-10-11 01:03:08 +0300

    part1

---
+++ Convolution Neural Networks/Convolution model_application/part1.py
@@ -0,0 +1,12 @@
+import math
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
+from scipy import ndimage
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from cnn_utils import *
+
+print("smth")
